fcbd48c100aef523dc93af01f870b7519e8ed642
Javier Dehesa <javidcf@gmail.com>
2016-04-27 23:30:10 +0100

    Some changes in basic assets.

--- static/js/game.js
+++ static/js/game.js
@@ -1,13 +1,22 @@
 
 window.onload = function() {
 
-    // TODO make this local later
-    game = new Phaser.Game(480, 640, Phaser.AUTO, '', { preload: preload, create: create });
+    var renderer = Phaser.CANVAS;  // Phaser.CANVAS / Phaser.WEBGL / Phaser.AUTO
+    var transparent = false;
+    var antialias = false;
+    var game = new Phaser.Game(480, 640, renderer, '',
+                               { preload: preload, create: create },
+                               transparent, antialias);
+    // Phaser.Canvas.setImageRenderingCrisp(game.canvas);
+    // PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
+    // Phaser.Canvas.setSmoothingEnabled(game.context, false);
 
     function preload () {
         game.load.baseURL = '/static/assets/';
         game.load.image('background', 'background.png');
-        game.load.spritesheet('host', 'palo.png', 111, 114);
+        game.load.spritesheet('host', 'palo.png', 30, 85);
+        game.load.image('dialogPane', 'dialog_pane.png');
+        game.load.image('optionsPane', 'options_pane.png');
     }
 
     function create () {
@@ -16,10 +25,18 @@ window.onload = function() {
         background.anchor.setTo(0.5, 0.5);
         background.scale.setTo(2, 2);
 
-        host = game.add.sprite(0, 0, 'host');
-        host.x = game.world.width - host.width / 2 - 40;
-        host.y = game.world.centerY + game.world.height / 4;
+        var optionsPane = game.add.sprite(game.world.centerX, game.world.height - 260, 'optionsPane');
+        optionsPane.anchor.setTo(0.5, 0.0);
+
+        var dialogPane = game.add.sprite(game.world.centerX, 220, 'dialogPane');
+        dialogPane.anchor.setTo(0.5, 0.0);
+
+        var host = game.add.sprite(0, 0, 'host');
+        // host.smoothed = false;  // does this do something?
+        host.x = game.world.width - host.width / 2 - 55;
+        host.y = game.world.height - 140;
         host.anchor.setTo(0.5, 0.5);
+        host.scale.setTo(2, 2);
 
     }
 
