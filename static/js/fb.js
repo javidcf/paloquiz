ab1090e1eeb5360e87b8c2860a36f888c0786ccf
Javier Dehesa <javidcf@gmail.com>
2016-05-01 20:04:46 +0100

    Refactored FB functions.

--- static/js/fb.js
+++ static/js/fb.js
@@ -1,5 +1,7 @@
 // Some functions to interact with Facebook
 
+var FB_PERMISSIONS = 'public_profile, user_friends, publish_actions';
+
 var FB_UID = undefined;
 var FB_ACCESS_TOKEN = undefined;
 
@@ -15,118 +17,105 @@ function fbIsLoggedIn() {
 }
 
 function fbPublishScore(score, callback, errorCallback) {
-    fbUseApi(function () {
-        FB.api('/' + FB_UID + '/scores', 'post',
-            {access_token: FB_ACCESS_TOKEN, score: score},
-            function (response) {
-                if (response.success) {
-                    if (callback) {
-                        callback(score);
-                    }
-                } else {
-                    console.error('Could not publish score to Facebook');
-                    if (errorCallback) {
-                        errorCallback();
-                    }
-                }
-            });
-    });
-}
-
-function fbGetUserScore(callback) {
-    fbUseApi(function () {
-        FB.api('/' + FB_UID + '/scores', 'get',
-            {access_token: FB_ACCESS_TOKEN},
-            function (response) {
-                data = response.data;
-                if (data.length < 1) {
-                    console.error('Could not retrieve the user score');
-                    return;
-                }
-                // Check if there are several scores in the response
-                var iScore = 0;
-                if (data.length > 1) {
-                    // Find the app score
-                    for (var i = 0; i < data.length; i++) {
-                        if (data[i].application.id == FB_APP_ID) {
-                            iScore = i;
-                            break;
-                        }
-                    }
-                }
-                // Get the score
-                var score = data[iScore].score;
+    fbUseApi('/' + FB_UID + '/scores', 'post', {},
+        function (response) {
+            if (response.success) {
                 if (callback) {
                     callback(score);
                 }
-            });
-    });
+            } else {
+                console.error('Could not publish score to Facebook');
+                if (errorCallback) {
+                    errorCallback();
+                }
+            }
+        });
 }
 
-function fbGetFriendsScores(callback) {
-    fbUseApi(function () {
-        FB.api('/' + FB_APP_ID + '/scores', 'get',
-            {access_token: FB_ACCESS_TOKEN},
-            function (response) {
-                data = response.data;
-                // Find user position
-                var userIdx = undefined;
+function fbGetUserScore(callback) {
+    fbUseApi('/' + FB_UID + '/scores', 'get', {},
+        function (response) {
+            data = response.data;
+            if (data.length < 1) {
+                console.error('Could not retrieve the user score');
+                return;
+            }
+            // Check if there are several scores in the response
+            var iScore = 0;
+            if (data.length > 1) {
+                // Find the app score
                 for (var i = 0; i < data.length; i++) {
-                    if (data[i].user.id == FB_UID) {
-                        userIdx = i;
+                    if (data[i].application.id == FB_APP_ID) {
+                        iScore = i;
                         break;
                     }
                 }
+            }
+            // Get the score
+            var score = data[iScore].score;
+            if (callback) {
+                callback(score);
+            }
+        });
+}
 
-                if (callback) {
-                    callback(data, userIdx);
+function fbGetFriendsScores(callback) {
+    fbUseApi('/' + FB_APP_ID + '/scores', 'get', {},
+        function (response) {
+            data = response.data;
+            // Find user position
+            var userIdx = undefined;
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].user.id == FB_UID) {
+                    userIdx = i;
+                    break;
                 }
-            });
-    });
+            }
+
+            if (callback) {
+                callback(data, userIdx);
+            }
+        });
 }
 
 function fbGetProfilePicture(uid, callback) {
-    fbUseApi(function () {
-        FB.api('/' + uid + '/picture', 'get',
-            {access_token: FB_ACCESS_TOKEN},
-            function (response) {
-                if (callback) {
-                    callback(response.data.url, response.data.url.is_silhouette);
-                }
-            });
-    });
+    fbUseApi('/' + uid + '/picture', 'get', {},
+        function (response) {
+            if (callback) {
+                callback(response.data.url, response.data.is_silhouette);
+            }
+        });
 }
 
 function fbGetUserProfilePicture(callback) {
-    fbUseApi(function () {
-        FB.api('/me/picture', 'get',
-            {access_token: FB_ACCESS_TOKEN},
-            function (response) {
-                if (callback) {
-                    callback(response.data.url, response.data.url.is_silhouette);
-                }
-            });
-    });
+    fbUseApi('/me/picture', 'get', {},
+        function (response) {
+            if (callback) {
+                callback(response.data.url, response.data.is_silhouette);
+            }
+        };);
 }
 
-function fbUseApi(callback) {
+function fbUseApi(url, method, params, callback) {
+    params = params || ({});
     if (fbIsLoggedIn()) {
         // Already logged in
-        callback();
+        params.access_token = FB_ACCESS_TOKEN;
+        FB.api(url, method, params, callback);
     } else {
         FB.getLoginStatus(function (response) {
             if (response.status === 'connected') {
                 // Already granted permissions
                 _fbSaveLogin(response);
-                callback();
+                params.access_token = FB_ACCESS_TOKEN;
+                FB.api(url, method, params, callback);
             } else {
                 // Show login dialog first
                 FB.login(function (response) {
                     _fbSaveLogin(response);
-                    if (callback) {
-                        callback();
-                    }
-                }, {scope: 'public_profile, user_friends, publish_actions'});
+                    params.access_token = FB_ACCESS_TOKEN;
+                    FB.api(url, method, params, callback);
+                }, {scope: FB_PERMISSIONS});
             }
         });
     }
