e40683c7c6c15fb224562df90a36e2db1ad8b09f
Javier Dehesa <javidcf@gmail.com>
2016-05-01 16:21:27 +0100

    Fixed method/methods typo

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ if __name__ != '__main__':
 # Application routes
 
 
-@app.route('/', method=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def root():
     return redirect(url_for('static', filename='index.html'))
 
