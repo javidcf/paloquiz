6613bad7cc083e39d9c9cf3c05a4cc18d4b3e098
kterry <karem.terry@unican.es>
2016-05-02 01:14:52 +0100

    Some code refactoring, async preload and testing webfonts.

---
+++ static/js/ajax.js
@@ -0,0 +1,35 @@
+function getJSON(url, callback, callbackError)
+{
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.open('GET', url, true);
+    xmlHttp.send( null );
+
+    var response = xmlHttp.responseText;
+
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4){
+            if(xmlHttp.status == 200) {
+                var jsonResponse = JSON.parse(xmlHttp.responseText);
+                if (callback) {
+                    callback(jsonResponse, xmlHttp.status);
+                }
+            }else{
+                try{
+                    errResponse = JSON.parse(response);
+                    console.error(errResponse['message']);
+                    if (callbackError){
+                        callbackError(errResponse['message'], xmlHttp.status);
+                    }
+                }catch(e){
+                    var errMsg = 'Unexpected error ('+ xmlHttp.status +')';
+                    console.error(errMsg);
+                    if (callbackError){
+                        callbackError(errMsg, xmlHttp.status);
+                    }
+                }
+            }
+        }
+    };
+}
--- static/js/game.js
+++ static/js/game.js
@@ -1,23 +1,34 @@
-
 window.onload = function() {
 
     var CONTAINER_ID = 'gameContainer';
     container = document.getElementById(CONTAINER_ID);
 
-    var renderer = Phaser.CANVAS;  // Phaser.CANVAS / Phaser.WEBGL / Phaser.AUTO
+    var renderer = Phaser.AUTO; // Phaser.CANVAS / Phaser.WEBGL / Phaser.AUTO
     var transparent = false;
     var antialias = false;
-    var game = new Phaser.Game(container.clientWidth, container.clientHeight,
-                               renderer, CONTAINER_ID,
-                               { preload: preload, create: create },
-                               transparent, antialias);
-    // Phaser.Canvas.setImageRenderingCrisp(game.canvas);
-    // PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
-    // Phaser.Canvas.setSmoothingEnabled(game.context, false);
+    game = new Phaser.Game(container.clientWidth, container.clientHeight,
+        renderer, CONTAINER_ID, {
+            init: init,
+            preload: preload,
+            create: create
+        },
+        transparent, antialias);
 
-    var qImageHeight = 210
+    var QUESTION_IMAGE_MAX_HEIGHT = game.height * .32;
+    var QUESTION_IMAGE_MAX_WIDTH = game.width * .8;
 
-    function preload () {
+    function init() {
+        fbInit();
+
+        // Does this do something?
+        PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
+        if (game.renderType == Phaser.CANVAS) {
+            Phaser.Canvas.setImageRenderingCrisp(game.canvas);
+            Phaser.Canvas.setSmoothingEnabled(game.context, false);
+        }
+    }
+
+    function preload() {
         game.load.baseURL = '/static/';
         game.load.image('background', 'assets/background.png');
         game.load.spritesheet('host', 'assets/palo.png', 30, 85);
@@ -28,7 +39,7 @@ window.onload = function() {
         game.load.bitmapFont('desyrel', 'assets/desyrel.png', 'assets/desyrel.xml');
     }
 
-    function create () {
+    function create() {
 
         var background = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
         background.anchor.setTo(0.5, 0.5);
@@ -54,219 +65,239 @@ window.onload = function() {
         game.load.onLoadComplete.add(loadComplete, this);
 
         // Score text
-        scoreText = game.add.text(game.world.centerX + game.world.width/2 - 70, 10, '0', { fill: '#ff8000' });
+        scoreText = game.add.text(game.world.centerX + game.world.width / 2 - 70, 10, '0', {
+            fill: '#ff8000'
+        });
 
         //  Progress report
-        loadText = game.add.text(game.world.centerX, 50, 'Image', { fill: '#ffffff' });
+        loadText = game.add.text(game.world.centerX, 50, 'Image', {
+            fill: '#ffffff'
+        });
 
         //  Bit map Text
-        
+
         //messageText = game.add.bitmapText(200, 100, 'desyrel', 'Phaser & Pixi\nrocking!', 64);
-        messageText = game.add.bitmapText(game.world.centerX,game.world.centerY, 'desyrel', 'Correct', 80);
+        messageText = game.add.bitmapText(game.world.centerX, game.world.centerY, 'desyrel', 'Correct', 80);
         messageText.inputEnabled = true;
         messageText.anchor.setTo(0.5, 0.5);
         messageText.visible = false
 
 
         // Question text
-        qTextY = dialogPane.y + dialogPane.height/2
-        questionText = game.add.text(game.world.centerX, qTextY, 'Question', { fill: '#ffffff' });
+        qTextY = dialogPane.y + dialogPane.height / 2
+        questionText = game.add.text(game.world.centerX, qTextY, 'Question', {
+            font: 'Pixel Art',
+            fontSize: '26px',
+            align: 'center',
+            fill: '#ffffff'
+        });
         questionText.anchor.setTo(0.5, 0.5);
 
         optButtonsGroup = game.add.group();
         optButtons = new Array();
 
         var bX = optionsPane.x - optionsPane.width * 0.10;
-        var bY = optionsPane.y * 1.12 ; 
+        var bY = optionsPane.y * 1.12;
         var bHeight = (optionsPane.height * 0.8) / 4;
-        
 
-        for(var i=0; i < 4; i++){
-            optButtons[i] = new LabelButton(game, bX, bY + bHeight*i, "button", "Option", actionOnClick, this, 2, 1, 0); 
+
+        for (var i = 0; i < 4; i++) {
+            optButtons[i] = new LabelButton(game, bX, bY + bHeight * i, 'button', 'Option', actionOnClick, this, 2, 1, 0);
             optButtons[i].height = bHeight;
             optButtons[i].width = optionsPane.width * 0.72;
-            optButtons[i].answerId = i; 
+            optButtons[i].answerId = i;
             optButtonsGroup.add(optButtons[i]);
         }
 
-        getJSON('/start');
         updateStatus();
 
     }
 
-    function answerCorrect(buttonId){
-        messageText.setText("Correct! :)");
+    function answerCorrect(buttonId) {
+        messageText.setText('Correct! :)');
         messageText.visible = true;
         enableInput(false);
-        game.time.events.add(Phaser.Timer.SECOND*2, function(){
+        game.time.events.add(Phaser.Timer.SECOND * 2, function() {
             messageText.visible = false;
             enableInput(true);
+            updateStatus();
         }, this);
 
     }
 
-    function answerWrong(buttonId){
-        messageText.setText("Wrong! >:(");
+    function answerWrong(buttonId) {
+        messageText.setText('Wrong! >:(');
         messageText.visible = true;
         enableInput(false);
-        game.time.events.add(Phaser.Timer.SECOND*2, function(){
+        game.time.events.add(Phaser.Timer.SECOND * 2, function() {
             messageText.visible = false;
             enableInput(true);
+            updateStatus();
         }, this);
     }
 
-    function finishGame(){
-        messageText.setText("Finish!!! Score = "+ scoreText.text);
+    function finishGame() {
+        messageText.setText('Score = ' + scoreText.text);
         messageText.visible = true;
         enableInput(false);
     }
 
-    function enableInput(enable){
+    function enableInput(enable) {
         game.input.disabled = !enable;
     }
 
 
-    function actionOnClick (button) {
-        var answerResponse = getJSON('/answer/'+button.answerId);
+    function actionOnClick(button) {
+        getJSON('/answer/' + button.answerId, function(answerResponse) {
 
-        if (answerResponse["correct"]){
-            answerCorrect(button.answerId);
-        }else{
-            answerWrong(button.answerId);
-        }
-
-
-        console.info(answerResponse);
-        updateStatus();
-    }
-
-    function getJSON(theUrl)
-    {
-        var xmlHttp = null;
-        xmlHttp = new XMLHttpRequest();
-
-        xmlHttp.open('GET', theUrl, false );
-        xmlHttp.send( null );
-
-        var response = xmlHttp.responseText;
-
-        if (xmlHttp.status == 200){
-            return JSON.parse(response);
-        }else{
-            try{
-                err_response = JSON.parse(response);
-                console.error(err_response['message']);
-            }catch(e){
-                console.error('Unexpected error ('+ xmlHttp.status +')');
+            if (answerResponse['correct']) {
+                answerCorrect(button.answerId);
+            } else {
+                answerWrong(button.answerId);
             }
-
-            return undefined;
-        }
+        });
     }
 
-    function loadImage(imageName){
+    function loadQuestionImage(imageName) {
 
         game.load.image('questionImage', 'questions/' + imageName);
         game.load.start();
-
     }
 
-
     function loadStart() {
-
-        loadText.setText("Loading ...");
-
+        loadText.setText('Loading ...');
     }
 
     //  This callback is sent the following parameters:
     function fileComplete(progress, cacheKey, success, totalLoaded, totalFiles) {
-        loadText.setText("File Complete: " + progress + "% - " + totalLoaded + " out of " + totalFiles);
-        var x = 0;
-        var y = 0;
-
-        var newImage = game.add.sprite(x, y, cacheKey);
-
-        newImage.anchor.setTo(0.5, 0.5);
-        var realHeight = newImage.height;
-        var resScale = realHeight / qImageHeight;
-
-        newImage.height = qImageHeight;
-        newImage.width = newImage.width / resScale;
-
-        newImage.x = game.world.centerX
-        newImage.y = newImage.height/2 + 5
+        loadText.setText('File Complete: ' + progress + '% - ' + totalLoaded + ' out of ' + totalFiles);
+        if (cacheKey == 'questionImage') {
+            replaceQuestionImage(cacheKey);
+        }
     }
 
     function loadComplete() {
-        loadText.setText("Loaded");
+        loadText.setText('Loaded');
     }
 
-    function loadQuestion(question){
-        //bmpText = game.add.bitmapText(10, 100, 'carrier_command',question,34);
-        questionText.setText(question)
+    function replaceQuestionImage(cacheKey) {
+        clearQuestionImage();
+
+        var newImage = game.add.sprite(0, 0, cacheKey);
+
+        newImage.anchor.setTo(0.5, 0.5);
+
+        var resScaleH = QUESTION_IMAGE_MAX_HEIGHT / newImage.height;
+        var resScaleW = QUESTION_IMAGE_MAX_WIDTH / newImage.width;
+        var resScale = Math.min(resScaleH, resScaleW);
+        newImage.height = newImage.height * resScale;
+        newImage.width = newImage.width * resScale;
+
+        newImage.x = game.world.centerX;
+        newImage.y = newImage.height / 2 + game.height * .03;
+
+        game.questionImage = newImage;
+
+        if (game.onQuestionImageReplaced) {
+            game.onQuestionImageReplaced();
+        }
     }
 
-    function loadAnswers(answers){
-        for(var i=0; i< answers.length; i++){
+    function clearQuestionImage() {
+        if (game.questionImage) {
+            game.questionImage.kill();
+            game.questionImage = undefined;
+        }
+    }
+
+    function loadQuestion(useHeader) {
+
+        clearQuestionImage();
+
+        if (useHeader) {
+            game.onQuestionImageReplaced = function() {
+                getJSON('/question', function(question) {
+                    questionText.setText(question['question']);
+                    loadAnswers(question['answers']);
+                });
+            }
+            getJSON('/questionHeader', function(questionHeader) {
+                // questionText.setText(questionHeader['question']);
+                loadQuestionImage(questionHeader['img']);
+            });
+        } else {
+            game.onQuestionImageReplaced = undefined;
+            getJSON('/question', function(question) {
+                questionText.setText(question['question']);
+                loadAnswers(question['answers']);
+                loadQuestionImage(question['img']);
+            });
+        }
+    }
+
+    function loadAnswers(answers) {
+        for (var i = 0; i < answers.length; i++) {
             optButtons[i].setLabel(answers[i])
         }
     }
 
-    function updateStatus(){
-        var gameStatus = getJSON('/status');
-        console.debug(gameStatus);
+    function updateStatus() {
+        getJSON('/status', function(gameStatus) {
+            scoreText.setText(gameStatus['score']);
+            if (gameStatus['status'] == 'start') {
+                startGame();
+            } else if (gameStatus['status'] == 'question') {
+                loadQuestion(true);
+            } else if (gameStatus['status'] == 'answer') {
+                loadQuestion(false);
+            } else if (gameStatus['status'] == 'finish') {
+                finishGame();
+            }
+        });
 
-
-        scoreText.setText(gameStatus['score']);
-
-        if(gameStatus['status']  == 'question'){
-            var questionHeader = getJSON('/questionHeader');
-            loadImage(questionHeader['img']);
-            loadQuestion(questionHeader['question']);
-
-            var question = getJSON('/question');
-            loadAnswers(question['answers']);
-        }else if (gameStatus['status']  == 'finish'){
-            console.info("finish")
-            finishGame();
-        }
-        
     }
 
-    var LabelButton = function(game, x, y, key, label, callback, callbackContext, overFrame, outFrame, downFrame, upFrame){
+    function startGame() {
+        getJSON('/start', updateStatus);
+    }
+
+    var LabelButton = function(game, x, y, key, label, callback, callbackContext, overFrame, outFrame, downFrame, upFrame) {
         Phaser.Button.call(this, game, x, y, key, callback, callbackContext, overFrame, outFrame, downFrame, upFrame);
-        //Label style    
-        this.style = { 'font': '18px Arial',        
-                       'fill': 'white'};   
+        //Label style
+        this.style = {
+            font: 'Pixel Art',
+            fontSize: '12px',
+            align: 'center',
+            fill: 'white'
+        };
 
-        this.anchor.setTo( 0.5, 0.5 );   
-        this.label = new Phaser.Text(game, 0, 0, label, this.style);   
+        this.anchor.setTo(0.5, 0.5);
+        this.label = new Phaser.Text(game, 0, 0, label, this.style);
 
-        //puts the label in the center of the button    
-        this.label.anchor.setTo( 0.5, 0.5 );    
-        this.addChild(this.label);    
-        this.setLabel( label );    
+        //puts the label in the center of the button
+        this.label.anchor.setTo(0.5, 0.5);
+        this.addChild(this.label);
+        this.setLabel(label);
 
-        //adds button to game    
-        game.add.existing( this );
+        //adds button to game
+        game.add.existing(this);
     };
 
     LabelButton.prototype = Object.create(Phaser.Button.prototype);
 
     LabelButton.prototype.constructor = LabelButton;
 
-    LabelButton.prototype.setLabel = function( label ) {       
+    LabelButton.prototype.setLabel = function(label) {
         this.label.setText(label);
     };
 
     function sleep(milliseconds) {
         var start = new Date().getTime();
         for (var i = 0; i < 1e7; i++) {
-            if ((new Date().getTime() - start) > milliseconds){
-              break;
+            if ((new Date().getTime() - start) > milliseconds) {
+                break;
             }
         }
     }
-
 
 };
