6720389a9cea13ffba3f616b24abd98f4e7b19bf
Javier Dehesa <javidcf@gmail.com>
2016-05-02 18:56:22 +0100

    Fixed init code

--- static/js/game.js
+++ static/js/game.js
@@ -6,20 +6,12 @@ var CONTAINER_ID = 'gameContainer';
 window.onload = function () {
     var container = document.getElementById(CONTAINER_ID);
 
+    // Check Facebook status
+    fbInit();
+
     var renderer = Phaser.AUTO; // Phaser.CANVAS / Phaser.WEBGL / Phaser.AUTO
     var transparent = false;
     var antialias = false;
-
-    function init() {
-            fbInit();
-
-            // Does this do something?
-            PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
-            if (game.renderType == Phaser.CANVAS) {
-                Phaser.Canvas.setImageRenderingCrisp(game.canvas);
-                Phaser.Canvas.setSmoothingEnabled(game.context, false);
-            }
-    }
 
     Paloquiz.game = new Phaser.Game(container.clientWidth, container.clientHeight,
         renderer, CONTAINER_ID, {
@@ -27,6 +19,12 @@ window.onload = function () {
         },
         transparent, antialias);
 
+    // Does this do something?
+    PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
+    if (Paloquiz.game.renderType == Phaser.CANVAS) {
+        Phaser.Canvas.setImageRenderingCrisp(game.canvas);
+        Phaser.Canvas.setSmoothingEnabled(game.context, false);
+    }
 
     Paloquiz.game.state.add('Main', Paloquiz.states.Main);
     Paloquiz.game.state.add('Highscores', Paloquiz.states.Highscores);
