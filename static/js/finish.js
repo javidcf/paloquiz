5b7c778dcff52ef7293119862db35fc812553676
Javier Dehesa <javidcf@gmail.com>
2016-05-04 22:39:14 +0100

    Implemented finish state

--- static/js/finish.js
+++ static/js/finish.js
@@ -1,5 +1,9 @@
 Paloquiz.states.Finish = function(game) {
     this.score = 0;
+    this.hiscoresButton;
+    this.hiscoresLabel;
+    this.exitButton;
+    this.exitLabel;
 }
 
 Paloquiz.states.Finish.prototype = {
@@ -18,6 +22,7 @@ Paloquiz.states.Finish.prototype = {
                 this.createUI();
                 fbLogIn(function() {
                     this.hiscoresButton.visible = true;
+                    this.hiscoresLabel.visible = true;
                     fbGetUserScore(function(currentScore) {
                         if (this.score > currentScore) {
                             fbPublishScore(currentScore);
@@ -31,11 +36,36 @@ Paloquiz.states.Finish.prototype = {
     shutdown: function() {},
 
     createScoreText: function() {;
+        var scoreBaseTextSize = Math.round(.06 * this.world.height);
+        var scoreTextColor = '#ff4f1a';
+
+        var scoreHeader = this.add.text(
+            this.world.centerX,
+            this.world.height * .2,
+            'Puntuación', {
+                font: 'Pixel Art',
+                fontSize: scoreBaseTextSize + 'px',
+                fill: scoreTextColor,
+                align: 'center'
+            });
+        scoreHeader.anchor.setTo(.5, 0);
+
+        var scoreText = this.add.text(
+            this.world.centerX,
+            this.world.height * .33,
+            this.score, {
+                font: 'Pixel Art',
+                fontSize: Math.round(scoreBaseTextSize * 2.2) + 'px',
+                fill: scoreTextColor,
+                align: 'center'
+            });
+        scoreText.anchor.setTo(.5, 0);
     },
 
     createUI: function() {
+        // Hiscores
         this.hiscoresButton =
-            this.add.button(this.world.centerX, this.world.height * .6,
+            this.add.button(this.world.centerX, this.world.height * .55,
                 'finishButton',
                 function() {
                     this.state.start('Highscores');
@@ -46,6 +76,7 @@ Paloquiz.states.Finish.prototype = {
         this.hiscoresButton.anchor.setTo(.5, 0);
         this.hiscoresButton.visible = false;
 
+        // Exit
         this.exitButton =
             this.add.button(this.hiscoresButton.x,
                 this.hiscoresButton.y + this.hiscoresButton.height * 1.1,
@@ -59,11 +90,27 @@ Paloquiz.states.Finish.prototype = {
         this.exitButton.width = this.hiscoresButton.width;
         this.exitButton.height = this.hiscoresButton.height;
         this.exitButton.anchor.setTo(.5, 0);
-    },
 
-    finishGame: function() {;
-    },
+        // Labels
+        var labelTextSize = Math.round(this.hiscoresButton.height / 3.0);
+        var labelStyle = this.positionTextStyle = {
+            font: 'Pixel Art',
+            fontSize: labelTextSize + 'px',
+            fill: 'white',
+            align: 'center'
+        };
 
-    submitScore: function() {;
+        this.hiscoresLabel = this.add.text(
+            this.world.centerX,
+            this.hiscoresButton.y + this.hiscoresButton.height / 2,
+            'Clasificación', labelStyle);
+        this.hiscoresLabel.anchor.setTo(.5, .5);
+        this.hiscoresLabel.visible = false;
+
+        this.exitLabel = this.add.text(
+            this.world.centerX,
+            this.exitButton.y + this.exitButton.height / 2,
+            'Salir', labelStyle);
+        this.exitLabel.anchor.setTo(.5, .5);
     }
 }
